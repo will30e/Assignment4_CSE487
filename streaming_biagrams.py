84bf23606fd599f7be4a1add27a71ecb981377b9
gjiwierzbicki <44582681+gjiwierzbicki@users.noreply.github.com>
2025-07-07 14:31:55 -0400

    Update streaming_biagrams.py

--- streaming_biagrams.py
+++ streaming_biagrams.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-import string
+import string, time
 
 
 translator = str.maketrans('', '', string.punctuation)
@@ -14,16 +14,21 @@ def make_bigrams(words):
              for i in range(len(words) - 1) ]
 
 if __name__ == "__main__":
-    # Step 1: Create SparkContext 
     sc = SparkContext(appName="BigramStream", master="local[2]")
     ssc = StreamingContext(sc, batchDuration=1)
     ssc.checkpoint("/tmp/spark_streaming_checkpoint")
 
-    # Step 2: Create DStream from TCP source
     lines = ssc.socketTextStream("localhost", 9999)
 
-    lines.pprint()
+    bigrams = lines.flatMap(lambda line: make_bigrams(normalize(line)))
 
+    bigrams_window = bigrams.window(windowDuration = 3, slideDuration=2)
+
+    bigram_counts = bigrams_window.reduceByKey(lambda a, b: a + b)
+
+    bigram_counts.pprint()
+
+    print("Listening on port 9999")
+    
     ssc.start()
     ssc.awaitTermination()
-    
